5bd6158e7f6a8eb4dd29a95e83c111c4aecf02ed
Nícolas Teixeira <nicolasteixeira3856@outlook.com>
2020-07-07 20:27:20 -0300

    Atualizando projeto em React, adicionando endpoint da Globo e começando desenvolvimento do layout

--- front-react/src/pages/Index/index.tsx
+++ front-react/src/pages/Index/index.tsx
@@ -1,19 +1,84 @@
-import React from 'react';
-import './styles.css'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-import logo from '../../assets/logo.svg';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import Container from '@material-ui/core/Container';
+import Accordion from '@material-ui/core/Accordion';
+import AccordionSummary from '@material-ui/core/AccordionSummary';
+import AccordionDetails from '@material-ui/core/AccordionDetails';
+import Typography from '@material-ui/core/Typography';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+
+import './index.css'
+
+import api from '../../services/api';
+
+
+interface Entries {
+    id: number,
+    title: string,
+    description: string,
+    human_start_time: string,
+    human_end_time: string,
+    duration_in_minutes: string
+}
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+      flexGrow: 1,
+      width: '100%',
+    },
+    paper: {
+      padding: theme.spacing(2),
+      textAlign: 'center',
+      color: theme.palette.text.secondary,
+    },
+    heading: {
+        fontSize: theme.typography.pxToRem(15),
+        fontWeight: theme.typography.fontWeightRegular,
+    },
+}));
 
 const Index = () => {
+
+    const classes = useStyles();
+
+    useEffect(() => {
+        axios.get("https://epg-api.video.globo.com/programmes/1337?date=2020-07-07").then(response => {
+            console.log(response);
+        })
+        .catch(error => {
+            console.log(error);
+        })
+    }, []);    
+
     return (
-        <div id="page-home">
-            <div className="content">
-                <header>
-                    <img src={logo} alt="Ecoleta"></img>
-                </header>
-                <main>
-                    <p>Hello World!</p>
-                </main>
-            </div>
+        <div className={classes.root}>
+            <Container maxWidth="md">
+                <Grid spacing={3}>
+                    <Grid item xs={12}>
+                        <h1>Teste Prático - RPC</h1>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <Accordion>
+                            <AccordionSummary
+                                expandIcon={<ExpandMoreIcon />}
+                                aria-controls="panel1a-content"
+                                id="panel1a-header">
+                                <Typography className={classes.heading}>Accordion 1</Typography>
+                            </AccordionSummary>
+                            <AccordionDetails>
+                                <Typography>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
+                                sit amet blandit leo lobortis eget.
+                                </Typography>
+                            </AccordionDetails>
+                        </Accordion>
+                    </Grid>
+                </Grid>
+            </Container>
         </div>
     )
 }
